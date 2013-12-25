7834fd6ad08e6bd368d115bc1fff4e6ae51587d1
Smita Busar <smitabusar@gmail.com>
2013-12-25 15:00:47 -0800

    linked list traversal, insert at head and delete

---
+++ js/linkedList.js
@@ -0,0 +1,79 @@
+var Node = function(value, next) {
+	this.data = value;
+	this.next = (next === undefined) ? null : next;
+}
+
+var LinkedList = function() {
+	this.start = null;
+}
+
+LinkedList.prototype.print = function() {
+	var temp = this.start;
+	var result = "[";
+	while (temp != null) {
+		result += (temp.next !== null) ? (temp.data + " , ") : (temp.data);
+		temp=temp.next;
+	}
+	result += "]";
+	return result;
+}
+
+LinkedList.prototype.insert = function(value) {
+	if (value === undefined) {
+		return -2;
+	}
+	var next=this.start;
+	this.start= new Node(value, next);
+	//console.log(this.start);
+}
+
+LinkedList.prototype.delete = function(value) {
+	if (value === undefined) {
+		return -2;
+	}
+	var current = this.start;
+	var previous = null;
+	var found = false;
+	while (current !== null && !found) {
+		if (current.data === value) {
+			found = true;
+		} else {
+			previous = current;
+			current = current.next;
+
+		}
+	}
+	//console.log(found,current,previous);
+	if (found) {
+		if (previous === null) {
+			this.start = this.start.next;
+		} else {
+			previous.next = current.next;
+		}
+		//in C have delete operation for current node;
+		return 1;
+	} else {
+		return -1;
+	}
+}
+
+var ll = new LinkedList();
+console.log("print empty list " + ll.print());
+console.log("delete on empty list " + ll.delete(1));
+console.log("delete with no parameter " + ll.delete());
+console.log("insert with no parameter " + ll.insert());
+ll.insert(9);
+console.log("inserted 1 element " + ll.print());
+ll.insert(15);
+console.log("inserted another element " + ll.print());
+ll.insert(12);
+console.log("inserted another element " + ll.print());
+ll.insert(3);
+console.log("inserted another element " + ll.print());
+console.log("delete with value not found " + ll.delete(1));
+ll.delete(3);
+console.log("deleted 1st element " + ll.print());
+ll.delete(15);
+console.log("deleted middle element " + ll.print());
+ll.delete(9);
+console.log("deleted last element " + ll.print());
